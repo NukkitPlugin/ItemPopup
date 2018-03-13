9f982b6873aab2cb32a16c3cec3cfcee1f94b24e
PresentKim <qjrmckd@gmail.com>
2018-03-13 22:33:07 +0900

    ItemPopup : Remove debug on load,enable,disable

--- src/main/java/kim/present/nukkit/itempopup/ItemPopup.java
+++ src/main/java/kim/present/nukkit/itempopup/ItemPopup.java
@@ -1,21 +1,14 @@
 package kim.present.nukkit.itempopup;
 
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.TextFormat;
 
 public class ItemPopup extends PluginBase {
-    @Override
-    public void onLoad() {
-        this.getLogger().info(TextFormat.DARK_PURPLE + "onLoad");
-    }
 
     @Override
     public void onEnable() {
-        this.getLogger().info(TextFormat.DARK_GREEN + "onEnable");
     }
 
     @Override
     public void onDisable() {
-        this.getLogger().info(TextFormat.DARK_RED + "onDisable");
     }
 }
